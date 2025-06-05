99fd0ec284f7ccb5d2a781a4e35af0e74cdcc9c3
Luis Goncalves <154645283+LLuisGL@users.noreply.github.com>
2025-06-05 11:26:01 -0500

    Initial commit

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,19 @@
+<nav class='flex justify-center w-screen bg-gray-100'>
+    <div class='flex flex-row justify-around items-center w-2/3'>
+        <a href="/">
+            <svg height="48" viewBox="0 0 17 48" width="17" xmlns="http://www.w3.org/2000/svg"><path d="m15.5752 19.0792a4.2055 4.2055 0 0 0 -2.01 3.5376 4.0931 4.0931 0 0 0 2.4908 3.7542 9.7779 9.7779 0 0 1 -1.2755 2.6351c-.7941 1.1431-1.6244 2.2862-2.8878 2.2862s-1.5883-.734-3.0443-.734c-1.42 0-1.9252.7581-3.08.7581s-1.9611-1.0589-2.8876-2.3584a11.3987 11.3987 0 0 1 -1.9373-6.1487c0-3.61 2.3464-5.523 4.6566-5.523 1.2274 0 2.25.8062 3.02.8062.734 0 1.8771-.8543 3.2729-.8543a4.3778 4.3778 0 0 1 3.6822 1.841zm-6.8586-2.0456a1.3865 1.3865 0 0 1 -.2527-.024 1.6557 1.6557 0 0 1 -.0361-.337 4.0341 4.0341 0 0 1 1.0228-2.5148 4.1571 4.1571 0 0 1 2.7314-1.4078 1.7815 1.7815 0 0 1 .0361.373 4.1487 4.1487 0 0 1 -.9867 2.587 3.6039 3.6039 0 0 1 -2.5148 1.3236z"></path></svg>
+        </a>
+        <a href="" class='text-gray-800 text-[12px] font-sans'>Mac</a>
+        <a href="" class='text-gray-800 text-[12px] font-sans'>iPad</a>
+        <a href="" class='text-gray-800 text-[12px] font-sans'>iPhone</a>
+        <a href="" class='text-gray-800 text-[12px] font-sans'>Watch</a>
+        <a href="" class='text-gray-800 text-[12px] font-sans'>AirPods</a>
+        <a href="" class='text-gray-800 text-[12px] font-sans'>TV y Casa</a>
+        <a href="" class='text-gray-800 text-[12px] font-sans'>Entretenimiento</a>
+        <a href="" class='text-gray-800 text-[12px] font-sans'>Soporte</a>
+        <a href="" class='text-gray-800 text-[12px] font-sans'>Dónde Comprar</a>
+        <a href="">
+            <svg height="44" viewBox="0 0 15 44" width="15" xmlns="http://www.w3.org/2000/svg"><path d="m14.2655 27.0566-3.9155-3.9089a5.4578 5.4578 0 1 0 -1.1123 1.15l3.8966 3.8905a.8.8 0 1 0 1.1309-1.1319zm-12.6655-7.0542a4.3328 4.3328 0 1 1 4.333 4.3257 4.3342 4.3342 0 0 1 -4.333-4.3257zm12.666 7.0542-3.916-3.9089a5.4578 5.4578 0 1 0 -1.1123 1.15l3.8966 3.8905a.8.8 0 1 0 1.1309-1.1319zm-12.666-7.0542a4.3328 4.3328 0 1 1 4.333 4.3257 4.3342 4.3342 0 0 1 -4.333-4.3257z"></path></svg>
+        </a>
+    </div>
+</nav>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- FONTS -->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    <!-- -->
+
+    <style>
+        body {
+            font-family: 'Open Sans', sans-serif;
+        }
+    </style>
+    
+    <title>Apple</title>
+</head>
+<body class='antialiased'>
+    <main class="">
+        @include('components.navbar')
+    </main>
+</body>
+</html>
