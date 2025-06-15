2ac73af77af7a30cc4e26f6d9d0572588a59473b
carloselcrackt1 <carlosalejandroauquimolina@gmail.com>
2025-06-15 16:08:56 -0500

    feat:  Login include the footer
    Agregamos el footer al login e hicimos algunos cambios en el padding de  ciertos contenedores

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -18,74 +18,83 @@
 
     <title> Inicio de sesion </title>
 </head>
-<body class='antialiased' background-color= 'lightblue';>
+<body class='antialiased bg-[#181A21]'>
     
-    <header>
-        @include('components.navbar')
-    </header>
+        <header>
+            @include('components.navbar')
+        </header>
 
-    <main class ="w-screen h-screen">
-        <div  class = "bg-[url('/img/steamFondo.jpeg')] bg-no-repeat bg-cover w-full h-full  items-center">
+        <main class ="w-full h-full">
+            <div  class = "bg-[url('/img/steamFondo.jpeg')] bg-no-repeat bg-cover w-full h-full  items-center">
 
-            <div class ='pt-[80px] pb-[150px] items-center flex flex-col'>
+                <div class ='pt-[100px] pb-[200px] items-center flex flex-col'>
 
-                <div class='flex flex-col gap-2 my-2 mx-4 pr-[320px] pb-[20px]'>
-                    <div class= 'text-[#FFFFFF] font-motiva text-3xl font-extrabold'>
-                    Inicio de sesión
+                    <div class='flex flex-col gap-2 my-2 mx-4 pr-[320px] pb-[20px]'>
+                        <div class= 'text-[#FFFFFF] font-motiva text-3xl font-extrabold'>
+                        Inicio de sesión
+                        </div>
+                    </div>
+
+                    <div class = "bg-[#181A21] rounded-[4px] p-[32px_40px] flex-1 min-w-[636px]">
+                        <form method='POST' action= "{{route('inicia-sesion')}}">
+                            @csrf
+                            
+                            <div class = "flex flex-col" >
+                                <label for="name" class="form-label text-[#1999ff] font-motiva font-medium ">INICIA SESIÓN CON TU NOMBRE DE CUENTA</label>
+                                <input 
+                                    type="text" 
+                                    class="bg-[#393c44] border border-[#393c44] hover:brightness-125 text-white px-3 py-2 rounded outline-none focus:bg-[#393c44] focus:border-[#393c44] focus:brightness-125 transition duration-200"
+                                    id="name" name="name" value="{{old('name')}}"
+                                    />
+                            </div>
+                        
+                            <div class= "flex flex-col pt-[10px]">
+                                <label for="password" class="form-label" style ="color: #AFAFAF">CONTRASEÑA</label>
+                                <input 
+                                    type="password" 
+                                    class="bg-[#393c44] border border-[#393c44] hover:brightness-125 text-white px-3 py-2 rounded outline-none focus:bg-[#393c44] focus:border-[#393c44] focus:brightness-125 transition duration-200"
+                                    id="password" name='password'
+                                />
+                            </div>
+
+                            <div class="flex flex-row  pt-[10px]">
+                                <input type="checkbox" class="form-check-input bg-[#393c44] " id="exampleCheck1">
+                                <label class="form-check-label pl-[5px] text-[#AFAFAF]" for="exampleCheck1">Recordarme</label>
+                            </div>
+                            
+                            <div class="bg-[#181A21] flex justify-center items-center h-[50px]" >
+                                <button type="submit"  class="bg-gradient-to-r from-[#06BFFF] to-[#2D73FF] text-white font-semibold  w-[270px] h-[40px]">Iniciar sesion</button>
+                            </div>
+
+                            <div class="bg-[#181A21] flex justify-center items-center h-[50px]">
+                                @if($errors->has('login_error'))
+                                    <label class="form-error-label text-[#C15755]">{{ $errors->first('login_error') }}</label>
+                                @endif
+                            </div>
+
+                            <a href="#" class="bg-[#181A2] text-[#AFAFAF] flex justify-center items-center h-[20px] underline hover:text-white" >Ayuda, no puedo iniciar sesion</a>
+
+
+                        </form>
                     </div>
                 </div>
-
-                <div class = "bg-[#181A21] rounded-[4px] p-[24px_32px] flex-1 min-w-[636px]">
-                    <form method='POST' action= "{{route('inicia-sesion')}}">
-                        @csrf
-                        
-                        <div class = "flex flex-col" >
-                            <label for="name" class="form-label text-[#1999ff] font-motiva font-medium ">INICIA SESIÓN CON TU NOMBRE DE CUENTA</label>
-                            <input 
-                                type="text" 
-                                class="bg-[#393c44] border border-[#393c44] hover:brightness-125 text-white px-3 py-2 rounded outline-none focus:bg-[#393c44] focus:border-[#393c44] focus:brightness-125 transition duration-200"
-                                id="name" name="name" value="{{old('name')}}"
-                                />
-                        </div>
-                    
-                        <div class= "flex flex-col pt-[10px]">
-                            <label for="password" class="form-label" style ="color: #AFAFAF">CONTRASEÑA</label>
-                            <input 
-                                type="password" 
-                                class="bg-[#393c44] border border-[#393c44] hover:brightness-125 text-white px-3 py-2 rounded outline-none focus:bg-[#393c44] focus:border-[#393c44] focus:brightness-125 transition duration-200"
-                                id="password" name='password'
-                            />
+                
+                <div class= 'flex justify-center items-center flex-row bg-[#181A21] pt-[48px] text-center'>
+                    <div class ='items-center h-[70px]'>
+                        <div class='pb-[10px]'>
+                            <h1 class="headline text-white text-[20px] text-center font-extrabold">¿Tu primera vez en Steam?</h1>
                         </div>
 
-                        <div class="flex flex-row  pt-[10px]">
-                            <input type="checkbox" class="form-check-input bg-[#393c44] " id="exampleCheck1">
-                            <label class="form-check-label pl-[5px] text-[#AFAFAF]" for="exampleCheck1">Recordarme</label>
-                        </div>
-                        
-                        <div class="bg-[#181A21] flex justify-center items-center h-[50px]" >
-                            <button type="submit"  class="bg-gradient-to-r from-[#06BFFF] to-[#2D73FF] text-white font-semibold  w-[270px] h-[40px]">Iniciar sesion</button>
-                        </div>
+                        <a href="/registro" 
+                        class="bg-gradient-to-r from-[#06BFFF] to-[#2D73FF] text-white text-center font-semibold p-[1px] w-[300px] h-20 m-[20px]" >
+                        <span class="px-[15px]">Crea una cuenta</span>
+                        </a>
 
-                        <div class="bg-[#181A21] flex justify-center items-center h-[50px]">
-                            @if($errors->has('login_error'))
-                                <label class="form-error-label text-[#C15755]">{{ $errors->first('login_error') }}</label>
-                            @endif
-                        </div>
-
-                        <a href="/registro" class="bg-[#181A2] text-[#AFAFAF] flex justify-center items-center h-[20px] underline hover:text-white" >No tienes cuenta todavia?</a>
-
-
-                    </form>
+                    </div>
+                    <div class='subtext pl-[10px] text-white w-[300px]'>Es gratis y muy fácil. Descubre miles de juegos para jugar con millones de amigos nuevos.</div>
                 </div>
+                @include('components.footer')
             </div>
-        </div>
-    </main> 
-    
-    <footer>
-        <div class= 'bg-[#181A21] text-[#AFAFAF] h-[290px]'>
-            este es el pie de pagina
-        </div>
-    </footer>
-
+        </main>   
 </body>
 </html>
