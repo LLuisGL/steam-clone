61a102c6e294c22d0f78da36e99859093123df9d
carloselcrackt1 <carlosalejandroauquimolina@gmail.com>
2025-06-15 16:07:24 -0500

    feat: Steam-footer
    Se acaba de crear el footer de steam que sera para usar en varias vistas

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,46 @@
+
+<div class= "bg-[#181A21] flex flex-col items-center justify-center pt-[20px]">
+
+    <div class='border-t-[1.5px] border-[#bababa] flex flew-row'>
+        <div class='h-[50px] w-[100px]  flex ustify-center items-center'>
+            <img src="./img/Logo_valve_new.png"
+            class ='object-scale-down' >
+        </div>
+
+        <div class='px-[475px]'></div>
+
+        <div class ='h-[50px] w-[50px]'>
+            <img src="/img/steam_icon.png" 
+            class= 'object-scale-down'>
+        </div>
+    </div>
+
+    <div class='border-b-[1.5px] border-[#bababa]'>
+        <div class='text-white'>
+            © 2025 Valve Corporation. Todos los derechos reservados. Todas las marcas registradas pertenecen a sus respectivos dueños en EE. UU. y otros países.
+        </div>
+        <div class='text-white flex justify-center pb-[5px]'>
+            Todos los precios incluyen IVA (donde sea aplicable).  
+        </div>
+    </div>
+</div>
+
+<div class='bg-[#181A21] flex flex-row justify-center pt-[10px]'> 
+    <div class='h-[50px] w-[215px] border-r-[1.5px] border-[#bababa] flex items-center flex-row'>
+        <div class= "h-[50px] w-[50px]">
+            <img src="./img/git.png" class= 'object-scale-down'>
+        </div>
+        <div class='text-center text-white underline pl-[5px] pr-[5px]'>
+            <a href="https://github.com/carloselcrackt1">Carlos Auqui Molina</a>
+        </div>
+    </div>
+
+    <div class='h-[50px] w-[225px] flex items-center flex-row pl-[5px]'>
+        <div class= "h-[50px] w-[50px]">
+            <img src="./img/git.png" class= 'object-scale-down'>
+        </div>
+        <div class='text-center text-white underline pl-[5px] pr-[5px]'>
+            <a href="https://github.com/LLuisGL">Luis Goncalves Lopez</a>
+        </div>
+    </div>
+</div>
