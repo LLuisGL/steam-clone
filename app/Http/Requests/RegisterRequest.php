40bae16cb201443d16c7209156a89247bee2636c
carloselcrackt1 <carlosalejandroauquimolina@gmail.com>
2025-06-16 22:29:59 -0500

    feat: Register view and Controllers
    En este commit se realizo la vista de register y se elaboro el controlodaro para la vista, ademas de agregar una imagen

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -26,6 +26,7 @@ class RegisterRequest extends FormRequest
         return [
             'email' => 'required|string|email|max:255|unique:users',
             'name' => 'required|string|max:30|unique:users',
+            'username'=>'required|string|max:30|unique:users',
             'password' => 'required|string|min:8',
         ];
     }
