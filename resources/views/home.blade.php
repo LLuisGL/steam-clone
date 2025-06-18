fc0de68ae0c4280e1b39b322559bc33f9cb5ce0e
LLuisGL <LuisGoncalvesLopez@gmail.com>
2025-06-18 02:04:48 -0500

    feat: Complete design of mini-cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -143,36 +143,48 @@
             </div>
             -->
             <div class="grid grid-cols-8 mb-4">
-                <div class="col-span-5 grid grid-cols-8 bg-red-100">
+                <div class="flex flex-col col-span-5 gap-2">
                     @foreach($juegos as $juego)
-                        <div class="col-span-3 h-full ">
-                            <img src="/img/{{$juego->id}}/{{$juego->imagenes->first()->url}}" alt="{{$juego->imagenes->first()->url}}" class="w-full h-24 object-cover">
-                        </div>
-                        <div class="col-span-5 grid grid-cols-5 h-full">
-                            @php
-                                $precio_oferta = $juego->precio_oferta > 0;
-                            @endphp
-
-                            <div class="col-span-{{ $precio_oferta ? 3 : 4 }}">
-                                <p>{{$juego->nombre_juego}}</p>
-                                <div class="flex flex-row gap-2">
-                                    @foreach($plataformas as $plataforma)
-                                        <img src="/img/platforms/{{$plataforma->url_imagen}}" alt="{{$plataforma->url_imagen}}">
-                                    @endforeach
-                                </div>
-                                <p>Tags</p>
+                        <div class="grid grid-cols-8 h-24">
+                            <div class="col-span-2 h-full bg-[#203647]">
+                                <img src="/img/{{$juego->id}}/{{$juego->imagenes->first()->url}}" alt="{{$juego->imagenes->first()->url}}" class="w-full h-24 object-cover">
                             </div>
-                            @if($juego->precio_oferta > 0)
-                                <div class="flex h-full items-center justify-center ">
-                                    <p class="text-white bg-green-300 text-center px-2">-10%</p>
+                            <div class="col-span-6 grid grid-cols-5 h-full bg-[#203647] text-white">
+                                @php
+                                    $precio_oferta = $juego->precio_oferta > 0;
+                                @endphp
+
+                                <div class="col-span-{{ $precio_oferta ? 3 : 4 }} pl-2 pt-2 flex flex-col justify-center">
+                                    <p>{{$juego->nombre_juego}}</p>
+                                    <div class="flex flex-row gap-1">
+                                        @foreach($plataformas as $plataforma)
+                                            <img src="/img/platforms/{{$plataforma->url_imagen}}" class="opacity-50" alt="{{$plataforma->url_imagen}}">
+                                        @endforeach
+                                    </div>
+                                    <p class="text-gray-400 text-sm">Tags</p>
                                 </div>
-                            @endif
-                            <div>
                                 @if($juego->precio_oferta > 0)
-                                    <p>precio_oferta</p>
+                                    <div class="flex h-full items-center justify-center ">
+                                        <p class="text-[#afdf13] font-bold bg-[#4c6b22] text-center px-2">-10%</p>
+                                    </div>
                                 @endif
-                                <p>precio_normal</p>
-                                <p>fecha</p>
+                                <div class="text-m flex flex-col justify-evenly pr-2">
+                                    <div class="h-1"></div>
+                                    <div>
+                                        @php
+                                            $precio_oferta = number_format($juego->precio_oferta,2);
+                                            $precio_normal = number_format($juego->precio_normal,2);
+                                        @endphp
+                                        @if($juego->precio_oferta > 0)
+                                            <p class="text-right text-gray-600 text-xs line-through">${{$precio_normal}}</p>    
+                                            <p class="text-right text-[#afdf13]">${{$precio_oferta}}</p>
+                                        @else
+                                            <p class="text-right">${{$precio_normal}}</p>    
+                                        @endif
+                                        
+                                    </div>
+                                    <p class="text-gray-400 text-xs uppercase text-right">{{ \Carbon\Carbon::parse($juego->created_at)->locale('es')->translatedFormat('d M Y') }}</p>
+                                </div>
                             </div>
                         </div>
                     @endforeach
