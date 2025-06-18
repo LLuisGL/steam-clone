959455855660d090e44d42d1c345861f6e0e2648
carloselcrackt1 <carlosalejandroauquimolina@gmail.com>
2025-06-18 11:36:16 -0500

    feat: first try of Profile_configuration
    Vista y controlador para el manejo del proceso de configuracion de usuario, tener en cuenta que aun no esta completo.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\cuentaController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\JuegosController;
@@ -27,3 +28,4 @@ Route::get('/registro',[RegisterController::class,'show']);
 Route::post('/validar-registro',[RegisterController::class,'register'])->name('validar-registro');
 Route::post('/inicia-sesion',[LoginController::class,'login'])->name('inicia-sesion');
 Route::get('/logout',[LogoutController::class,'logout'])->name('logout');
+Route::get('/cuenta',[cuentaController::class,'show']);
