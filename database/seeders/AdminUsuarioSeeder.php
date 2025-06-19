405eee3872e5e63ea294e60a327d88dd308445c3
carloselcrackt1 <carlosalejandroauquimolina@gmail.com>
2025-06-19 02:31:02 -0500

    feat: Admin seeder
    Se creo el seeder de admin

---
+++ database/seeders/AdminUsuarioSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Support\Facades\Hash;
+
+use Illuminate\Database\Seeder;
+
+class AdminUsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $rolAdmin = Role::where('role', 'ADMINISTRADOR')->first();
+        if (!$rolAdmin) {
+            $rolAdmin = Role::create([
+                'nombre' => 'ADMINISTRADOR',
+                
+            ]);
+        }
+
+        $usuarioAdmin = User::create([
+            'name' => 'Administrador',
+            'username' => 'AdminUees1',
+            'email' => 'admin@uees.com',
+            'password' => Hash::make('Admin123'), 
+        ]);
+
+        $usuarioAdmin->roles()->attach($rolAdmin->id);
+    }
+}
